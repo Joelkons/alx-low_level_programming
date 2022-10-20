6b21346a725e7451194930e41f90c34818168aa4
Joelkons <konsenj@gmail.com>
2022-10-19 19:57:51 -0700

    14-16

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci num.
+ * @void: numbers must be separated by comma.
+ * return: 0.
+ */
+
+int main(void)
+{
+	unsigned long count, a, b, c;
+
+	a = 0;
+	b = 1;
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu", c);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
