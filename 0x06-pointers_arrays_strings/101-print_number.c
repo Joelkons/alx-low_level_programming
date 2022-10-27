5cb01c6e1a2eb3ad158f201b6849b2778835a20e
Joelkons <konsenj@gmail.com>
2022-10-27 13:24:13 -0700

    task 09

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
+#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+
 /**
  * print_number -  prints an integer.
  * @n: num to be printed
+ * Return: void
  */
 
 void print_number(int n)
 {
-	unsigned int num;
-	if (n < 10 && n >= 0)
-	{
-		_putchar('0' + n);
-		return;
-	}
+	int i;
+	int p;
+
 	if (n < 0)
-	{
-		num = -n;
 		_putchar('-');
-	}
-	else
+	for (p = 0, i = 1000000000; i > 0; i /= 10)
 	{
-		num = n;
-	}
+		int d = (n / i) % 10;
 
-	if (num >= 10)
-	{
-		print_number(num / 10);
+		d = d < 0 ? -d : d;
+		if (d || p || i == 1)
+		{
+			_putchar(d + '0');
+			p++;
+		}
 	}
-	_putchar('0' + (num % 10));
 }
-
-
-
